1216f8daef84b9cccae56c87e48bea63f31ace32
Christopher <contato.christopher@gmail.com>
2024-10-22 11:42:28 -0300

    convert import components to functions

--- scripts/auto-import-components.js
+++ scripts/auto-import-components.js
@@ -1,65 +1,58 @@
-fetch('./components/header.html')
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById('componente-header').innerHTML = data;
+function importarComponente(componentPath, elementId, callback) {
+  fetch(componentPath)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(elementId).innerHTML = data;
+      if (callback) callback();
+    })
+    .catch((error) => console.error('Erro ao carregar o componente:', error));
+}
+
+importarComponente('components/header.html', 'componente-header');
+importarComponente('components/footer.html', 'componente-footer');
+importarComponente('components/login.html', 'componente-login', () => {
+  const btnAbrirModal = document.getElementById('loginRegistrar');
+  const modal = document.querySelector('.modal');
+  let status = 'closed';
+
+  btnAbrirModal.addEventListener('click', () => {
+    console.log('clicado');
+    modal.style.display = 'block';
+    status = 'open';
   });
 
-fetch('./components/footer.html')
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById('componente-footer').innerHTML = data;
-  });
+  function fecharModal() {
+    modal.style.display = 'none';
+  }
 
-fetch('./components/login.html')
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById('componente-login').innerHTML = data;
-
-    const btnAbrirModal = document.getElementById('loginRegistrar');
-    const modal = document.querySelector('.modal');
-    let status = 'closed';
-    btnAbrirModal.addEventListener('click', () => {
-      console.log('clicado');
-      console.log(modal);
-      console.log(modal.style);
-      modal.style.display = 'block';
-      status = 'open';
-    });
-
-    function fecharModal() {
-      modal.style.display = 'none';
+  document.addEventListener('click', (event) => {
+    if (
+      status === 'open' &&
+      !event.target.closest('.modal') &&
+      !event.target.closest('#loginRegistrar')
+    ) {
+      console.log('vou fechar');
+      status = 'closed';
+      fecharModal();
     }
-
-    document.addEventListener('click', (event) => {
-      if (
-        status === 'open' &&
-        !event.target.closest('.modal') &&
-        !event.target.closest('#loginRegistrar')
-      ) {
-        console.log('vou fechar');
-        status = 'closed';
-        fecharModal();
-      }
-    });
-
-    const cadastro = document.getElementById('cadastro-tab');
-    const login = document.getElementById('login-tab');
-
-    console.log('cadastro', cadastro);
-
-    cadastro.addEventListener('click', (event) => {
-      event.preventDefault();
-      document.getElementById('cadastro-tab-content').style.display = 'block';
-      document.getElementById('login-tab-content').style.display = 'none';
-      login.classList.remove('active');
-      cadastro.classList.add('active');
-    });
-
-    login.addEventListener('click', (event) => {
-      event.preventDefault();
-      document.getElementById('cadastro-tab-content').style.display = 'none';
-      document.getElementById('login-tab-content').style.display = 'block';
-      cadastro.classList.remove('active');
-      login.classList.add('active');
-    });
   });
+
+  const cadastro = document.getElementById('cadastro-tab');
+  const login = document.getElementById('login-tab');
+
+  cadastro.addEventListener('click', (event) => {
+    event.preventDefault();
+    document.getElementById('cadastro-tab-content').style.display = 'block';
+    document.getElementById('login-tab-content').style.display = 'none';
+    login.classList.remove('active');
+    cadastro.classList.add('active');
+  });
+
+  login.addEventListener('click', (event) => {
+    event.preventDefault();
+    document.getElementById('cadastro-tab-content').style.display = 'none';
+    document.getElementById('login-tab-content').style.display = 'block';
+    cadastro.classList.remove('active');
+    login.classList.add('active');
+  });
+});

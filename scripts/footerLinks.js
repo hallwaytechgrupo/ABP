7940c3c26b266ca8b9241e0efa4245040eabdfbc
ArielFagundes <arieldss6@gmail.com>
2024-10-26 00:12:01 -0300

    montagem footer sobre suporte localizacao part2

---
+++ scripts/footerLinks.js
@@ -0,0 +1,27 @@
+function sobreFunction() {
+    const sobreP = document.getElementById('sobreP');
+    if (sobreP.style.display === 'none') {
+        sobreP.style.display = 'block';
+    } else {
+        sobreP.style.display = 'none';
+    }
+}
+
+function suporteFunction() {
+    const suporteP = document.getElementById('suporteP');
+    if (suporteP.style.display === 'none') {
+        suporteP.style.display = 'block';
+    } else {
+        suporteP.style.display = 'none';
+    }
+}
+
+function localizacaoFunction() {
+    const localizacaoP = document.getElementById('localizacaoP');
+    if (localizacaoP.style.display === 'none') {
+        localizacaoP.style.display = 'block';
+    } else {
+        localizacaoP.style.display = 'none';
+    }
+}
+

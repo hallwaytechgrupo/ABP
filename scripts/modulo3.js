228c598e21211087d07dcd08ef75c5d653dd3e98
Christopher <contato.christopher@gmail.com>
2024-11-01 14:36:59 -0300

    fix directory

--- scripts/modulo3.js
+++ scripts/modulo3.js
@@ -1,12 +1,19 @@
 const accordions = document.querySelectorAll('.accordion');
 
-accordions.forEach(accordion => {
-    accordion.addEventListener('click', () => {
-        const body = accordion.querySelector('.accordion-body');
-        const header = accordion.querySelector('.accordion-header');
-        
-        // Alterna a classe 'active' para o conteúdo e o cabeçalho
-        body.classList.toggle('active');
-        header.classList.toggle('active');
-    });    
-});
+for (const accordion of accordions) {
+  accordion.addEventListener('click', () => {
+    for (const otherAccordion of accordions) {
+      if (otherAccordion !== accordion) {
+        const otherBody = otherAccordion.querySelector('.accordion-body');
+        const otherHeader = otherAccordion.querySelector('.accordion-header');
+        otherBody.classList.remove('active');
+        otherHeader.classList.remove('active');
+      }
+    }
+
+    const body = accordion.querySelector('.accordion-body');
+    const header = accordion.querySelector('.accordion-header');
+    body.classList.toggle('active');
+    header.classList.toggle('active');
+  });
+}

5c8afe52ca49a1befe0aa85e989cc1baa09eeb6c
YanBergantin <yanbergantin@outlook.com>
2024-11-13 20:27:36 -0300

    Update user.control.js
    função js para checar se o usuário está logado

--- scripts/utils/user.control.js
+++ scripts/utils/user.control.js
@@ -1,12 +1,22 @@
-const usuarioLogado = () => {
+// const usuarioLogado = () => {
+// Faça a tarefa aqui
+//  return true;
+//};
+
+//const setarLogado = (status) => {
   // Faça a tarefa aqui
-  return true;
-};
 
-const setarLogado = (status) => {
-  // Faça a tarefa aqui
+ // return status;
+//};
 
-  return status;
-};
+// export { usuarioLogado, setarLogado };
 
-export { usuarioLogado, setarLogado };
+function usuarioLogado() {
+    const logado = localStorage.getItem('logado');
+    return logado === 'true';
+}
+
+function setarLogado(status){
+    if (status === true || status === false) {
+        localStorage.setItem('logado', status.toString());
+    }

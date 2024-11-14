d05ff88786b25d842c55d2813791618eaf597b0b
Christopher <contato.christopher@gmail.com>
2024-11-14 14:02:51 -0300

    change to https

--- scripts/apiSetup.js
+++ scripts/apiSetup.js
@@ -3,7 +3,7 @@ const DOMAIN = 'abp-2024-2-server.onrender.com';
 let PORT;
 
 const api = axios.create({
-  baseURL: PORT ? `https://${DOMAIN}:${PORT}` : `http://${DOMAIN}`,
+  baseURL: PORT ? `https://${DOMAIN}:${PORT}` : `https://${DOMAIN}`,
 });
 
 export { api };

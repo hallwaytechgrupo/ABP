32b0fc7796b0abf5a7f14b8b4387e973ae12e01a
ArielFagundes <arieldss6@gmail.com>
2024-11-14 22:06:55 -0300

    criando função para cadastro

--- scripts/controllers/cadastro.controller.js
+++ scripts/controllers/cadastro.controller.js
@@ -1,3 +1,5 @@
+import {api} from './../apiSetup.js';
+
 export const clearData = (input1, input2) => {
   input1.value = '';
   input2.value = '';
@@ -25,3 +27,26 @@ export function getCadastroData() {
     confirmarSenha: confirmPassword,
   };
 }
+
+export const cadastro = async (nome, email, senha) => {
+  const usuario = {
+    name: nome,
+    mail: email,
+    password: senha
+  };
+
+  try {
+    const response = await api.post('/user/signup', usuario);
+    console.log('Cadastro realizado com sucesso:', response.data);
+    return { data: response.data, status: response.status };
+  } catch (error) {
+    if (error.response && error.response.status === 400) {
+      const { message } = error.response.data;
+      console.error('Erro de validação:', message);
+    } else {
+      console.error('Erro ao realizar cadastro:', error);
+    }
+      throw error;
+    }
+};
+

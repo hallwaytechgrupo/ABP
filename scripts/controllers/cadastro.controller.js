f7bdea2298064af6a736018507d97f99323c1cb9
ArielFagundes <arieldss6@gmail.com>
2024-11-15 19:36:03 -0300

    adicionando cadastro em .service.js

--- scripts/controllers/cadastro.controller.js
+++ scripts/controllers/cadastro.controller.js
@@ -28,25 +28,5 @@ export function getCadastroData() {
   };
 }
 
-export const cadastro = async (nome, email, senha) => {
-  const usuario = {
-    name: nome,
-    mail: email,
-    password: senha
-  };
 
-  try {
-    const response = await api.post('/user/signup', usuario);
-    console.log('Cadastro realizado com sucesso:', response.data);
-    return { data: response.data, status: response.status };
-  } catch (error) {
-    if (error.response && error.response.status === 400) {
-      const { message } = error.response.data;
-      console.error('Erro de validação:', message);
-    } else {
-      console.error('Erro ao realizar cadastro:', error);
-    }
-      throw error;
-    }
-};
 
--- scripts/services/cadastro.service.js
+++ scripts/services/cadastro.service.js
@@ -2,15 +2,25 @@ import { api } from './../apiSetup.js';
 
 export const cadastro = async (nome, email, senha) => {
   // Crie um objeto com os dados do usuário
-  const usuario = {};
+  const usuario = {
+    name: nome,
+    mail: email,
+    password: senha,
+  };
   try {
     // Faça uma requisição POST para a rota /user/signup
     const response = await api.post('/user/signup', usuario);
 
     // Retorne os dados da resposta
-    return {};
+    console.log('Cadastro realizado com sucesso:', response.data);
+    return { data: response.data, status: response.status };
   } catch (error) {
-    console.log(error);
+    if (error.response && error.response.status === 400) {
+      const { message } = error.response.data;
+      console.error('Erro de validação:', message);
+    } else {
+      console.error('Erro ao realizar cadastro:', error);
+    }
     throw error;
   }
 };

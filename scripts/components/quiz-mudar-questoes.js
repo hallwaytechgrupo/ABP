1b377e889e5a998777dec534b9fb393204ed578b
Christopher <contato.christopher@gmail.com>
2024-11-19 20:29:24 -0300

    add boilerplate for task

---
+++ scripts/components/quiz-mudar-questoes.js
@@ -0,0 +1,9 @@
+function prepararQuiz(modulo) {
+  const questao1Input = document.getElementById('question1');
+  const questao2Input = document.getElementById('question2');
+  const questao3Input = document.getElementById('question3');
+
+  // Obter questões por módulo
+}
+
+export default prepararQuiz;

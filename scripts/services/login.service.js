c03ab81d4a334501c35092a8cc387eda1b23eb1e
ArielFagundes <arieldss6@gmail.com>
2024-11-18 21:40:52 -0300

    adicionando função de login

--- scripts/services/login.service.js
+++ scripts/services/login.service.js
@@ -1,13 +1,28 @@
 import { api } from './../apiSetup.js';
 
 export const login = async (email, senha) => {
-  const usuario = {};
+  const usuario = {
+    email: email,
+    senha: senha
+  };
 
   try {
     const response = await api.post('/user/login', usuario);
 
-    return {};
+    console.log('Login realizado com sucesso:', response.data);
+
+    return { data: response.data, status: response.status };
+  
   } catch (error) {
-    console.log(error);
+
+    if (error.response && error.response.status === 400) {
+      const { message } = error.response.data;
+      console.error('Erro de validação:', message);
+      return message;
+    }
+
+    console.error('Erro ao realizar login:', error);
+    return error;
+
   }
 };

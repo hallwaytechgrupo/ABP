2dd331226152a10601ce895a3075209746ff3aea
Christopher <contato.christopher@gmail.com>
2024-11-21 19:16:55 -0300

    change import

--- scripts/components/quiz-mudar-questoes.js
+++ scripts/components/quiz-mudar-questoes.js
@@ -1,21 +1,21 @@
-import { getQuestoesByModulo } from "../utils/quiz.control.js";
+import { getQuestoesByModulo } from '../services/quiz.service.js';
 
 export function prepararQuiz(modulo) {
-   const questao1Input = document.getElementById('question1');
-   const questao2Input = document.getElementById('question2');
-   const questao3Input = document.getElementById('question3');
-  
-   const questoes = getQuestoesByModulo(modulo);
-  
-   if (questoes.length < 3) {
+  const questao1Input = document.getElementById('question1');
+  const questao2Input = document.getElementById('question2');
+  const questao3Input = document.getElementById('question3');
+
+  const questoes = getQuestoesByModulo(modulo);
+
+  if (questoes.length < 3) {
     console.error(`Não há questões suficientes para o módulo ${modulo}`);
     return;
-   }
-  
-   const [questao1, questao2, questao3] = questoes;
-   questao1Input.textContent = questao1.enunciado;
-   questao2Input.textContent = questao2.enunciado;
-   questao3Input.textContent = questao3.enunciado;
   }
+
+  const [questao1, questao2, questao3] = questoes;
+  questao1Input.textContent = questao1.enunciado;
+  questao2Input.textContent = questao2.enunciado;
+  questao3Input.textContent = questao3.enunciado;
+}
 
 export default prepararQuiz;
